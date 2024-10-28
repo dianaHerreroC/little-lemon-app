51fb7fdd7c19e2aac8b3c9b66905b182193cf743
Diana <d.herreroc@alumnos.upm.es>
2024-10-28 18:53:41 +0100

    styled about section and final styling of homepage

--- little-lemon-app/src/AboutSection.js
+++ little-lemon-app/src/AboutSection.js
@@ -1,17 +1,20 @@
+import './AboutSection.css'
 import chefsImageA from './assets/Mario and Adrian A.jpg';
 import chefsImageB from './assets/Mario and Adrian b.jpg';
 
-
-
 function AboutSection(){
     return(
-        <section>
-            <h1>Little Lemon</h1>
-            <h2>Chicago</h2>
-            <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. </p>
-            <img src={chefsImageA} alt="Restaurant chefs" style={{ maxHeight: '30vh' }}></img>
-            <img src={chefsImageB} alt="Restaurant chefs" style={{ maxHeight: '30vh' }}></img>
+        <section className='AboutSection'>
+            <span className='TextContainer'>
+                <h1>Little Lemon</h1>
+                <h2>Chicago</h2>
+                <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. </p>
+            </span>
+            <span className='ImageContainer'>
+                <img src={chefsImageA} alt="Restaurant chefs"></img>
+                <img src={chefsImageB} alt="Restaurant chefs"></img>
+            </span>
         </section>
     );
 };
--- little-lemon-app/src/HeroSection.js
+++ little-lemon-app/src/HeroSection.js
@@ -5,15 +5,15 @@ import foodImage from './assets/restauranfood.jpg';
 function HeroSection(){
     return(
         <section className="HeroSection">
-            <div className="LeftSection">
+            <span className="LeftSection">
                 <h1>Little Lemon</h1>
                 <h2>Chicago</h2>
                 <p>We are a familly owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
                 <Button>Reserve a table</Button>
-            </div>
-            <div className="RightSection">
+            </span>
+            <span className="RightSection">
                 <img src={foodImage} alt="Restaurant food"></img>
-            </div>
+            </span>
         </section>
     );
 };
