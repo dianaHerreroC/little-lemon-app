48252d7ef8078830d86be4b9daf2cd2377ed6a83
Diana <d.herreroc@alumnos.upm.es>
2024-11-04 11:32:06 +0100

    Updated the tests for initializeTimes and updateTimes functions

--- little-lemon-app/src/App.test.js
+++ little-lemon-app/src/App.test.js
@@ -10,16 +10,19 @@ test('renders the BookingForm heading', () => {
 
 describe("initializeTimes", () => {
   test("should return the correct initial times", () => {
-      const expectedTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
-      expect(initializeTimes()).toEqual(expectedTimes);
+    const initTimes = initializeTimes();
+    expect(Array.isArray(initTimes)).toBe(true);
+    expect(initTimes.length).toBeGreaterThan(0);
   });
 });
 
 describe("updateTimes", () => {
-  test("should return the same state if no logic is applied", () => {
-      const initialState = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
-      const date = {};  // Vacío por ahora, ya que la lógica se añadirá más adelante
-
-      expect(updateTimes(initialState, date)).toEqual(initialState);
+  test("should return a new state", () => {
+    const initialState = ["17:00", "18:00", "19:00"];
+    const date = {type:"2024-12-25"};
+    const avTimes = updateTimes(initialState, date);
+    expect(Array.isArray(avTimes)).toBe(true);
+    expect(avTimes.length).toBeGreaterThan(0);
+    expect(avTimes).not.toEqual(initialState);
   });
 });
--- little-lemon-app/src/BookingPage.js
+++ little-lemon-app/src/BookingPage.js
@@ -15,14 +15,7 @@ export const initializeTimes  = ()=>{
 function BookingPage(props){
     const [availableTimes, dispatchAT]=useReducer(updateTimes, initializeTimes());
     return(
-        <>
-            {/*<h1>Booking page</h1>
-            <h1>Display title = h1</h1>
-            <h2>Sub title = h2</h2>
-            <h3>Card title = h3</h3>
-            <p>Body = p</p>*/}
-            <BookingForm availableTimes={availableTimes} dispatchAT={dispatchAT} submitForm={props.submitForm}></BookingForm>
-        </>
+        <BookingForm availableTimes={availableTimes} dispatchAT={dispatchAT} submitForm={props.submitForm}></BookingForm>
     );
 };
 export default BookingPage;
